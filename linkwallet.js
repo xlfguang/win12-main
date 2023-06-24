dc338014d8997718849c0aec78888157d29e9482
AaronMRui <aaron1234156@outlook.com>
2023-06-24 23:40:51 +0800

    linkwallet

--- linkwallet.js
+++ linkwallet.js
@@ -1,5 +1,70 @@
-function linkwallet() {
+
+var web3je = "./js/web3.min.js";
+document.write('<scr' + 'ipt type="text/javascript" src="'+web3je+'"></scr' + 'ipt>');
+var etherje = "./js/ether.js";
+document.write('<scr' + 'ipt type="text/javascript" src="'+etherje+'"></scr' + 'ipt>');
+
+var walletWithProvider ;
+var privateAddress;
+var inputPrivatekey;
+var currentAddress ;
+
+
+async function linkwallet() {
   console.log("linkwallet");
+  var web3Provider;
+  if (window.ethereum) {
+      web3Provider = window.ethereum;
+      try {
+          await window.ethereum.enable();
+      } catch (error) {
+          console.error("User denied account access")
+      }
+  } else if (window.web3) {   
+      web3Provider = window.web3.currentProvider;
+  } else {
+      alert("！")
+      web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+  }
+  web3 = new Web3(web3Provider);
+  var id =  await web3.eth.net.getId()
+  if(id !=42161){
+    var rpc = {
+      chainId: '0xa4b1',
+      chainName: 'Arbitrum One',
+      nativeCurrency: {
+          name: 'ETH',
+          symbol: 'ETH',
+          decimals: 18,
+      },
+      rpcUrls: ['https://arb1.arbitrum.io/rpc'],
+      blockExplorerUrls: ['https://arbiscan.io'],
+  }
+  window.ethereum.request({
+      method: 'wallet_addEthereumChain', 
+      params: [{
+          chainId: rpc.chainId, 
+          chainName: rpc.chainName, 
+          rpcUrls: [
+              rpc.rpcUrls[0], 
+          ],
+          iconUrls: [
+              'https://testnet.hecoinfo.com/favicon.png' 
+          ],
+          blockExplorerUrls: [
+              rpc.blockExplorerUrls[0] 
+          ],
+          nativeCurrency: rpc.nativeCurrency
+      }]
+  })
+  }
+  let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  walletWithProvider = provider.getSigner();
+ 
+  privateAddress = await walletWithProvider.getAddress();
+ 
+  currentAddress = privateAddress.slice(0,4)+"XXXXX"+privateAddress.slice(-4);
+  linked(currentAddress);
 }
 
 function formatString(s, headLen = 5, tailLen = 4, ellipsis = "...") {
@@ -9,20 +74,16 @@ function formatString(s, headLen = 5, tailLen = 4, ellipsis = "...") {
     return s.slice(0, headLen) + ellipsis + s.slice(-tailLen);
   }
 }
-// 已经链接了钱包
-function linked() {
+
+function linked(adr) {
   let noneLink = document.getElementById("noneLink");
   noneLink.style.display = "none";
   let wallet = document.getElementById("linked");
   wallet.style.display = "flex";
   let address = document.getElementById("address");
-  // formatString函数中填写钱包地址
-  address.innerHTML = formatString("0x8FA0SSSSSS0000");
+  address.innerHTML = formatString(adr);
 }
-/**
- * 未链接钱包
- * 每次初始化都会执行noneLink 可以另外写函数来判断是否已经链接钱包 有则执行linked 没有则执行noneLink
- * */
+
 function noneLink() {
   let noneLink = document.getElementById("noneLink");
   noneLink.style.display = "block";
