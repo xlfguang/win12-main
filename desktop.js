eedd4a85b69fbe5862898aec78f2bc38c67830b6
xulf <2684695148@qq.com>
2023-06-25 14:37:25 +0800

    set url

--- desktop.js
+++ desktop.js
@@ -2385,7 +2385,7 @@ function opneLink(link, isCrossDomain = false) {
         window.open("https://tokensniffer.com/");
         break;
       case "discord":
-        window.open("https://discord.gg/cAW8e5cDEF");
+        window.open("https://discord.gg/NXS5U57PFg");
         break;
       case "youtube":
         window.open("https://www.youtube.com/");
